a67bd613e60e71519d6a3096015a62dbff82b498
Reinaldo Freitas <reinaldo.freitas@vogeltelecom.com>
2018-09-05 10:33:08 -0300

    testes

---
+++ src/screens/pessoas/clientes/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class About extends React.Component {
+	render() {
+		return (
+			<div id="container-fluid">
+				opa
+			</div>
+    	);
+	}
+}
+
+export default About;
